cd15bd8ba97841d70b7f3061212d52c79ac05b0a
nguyenviettungg <nguyentung696969@gmail.com>
2021-03-05 16:24:47 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -2,22 +2,22 @@ import React from "react";
 // import Accordion from "./components/accordion.component";
 import Search from "./components/search.component";
 
-const items = [
-  {
-    title: "what is React ?",
-    content: "react is a front end js framework",
-  },
-  {
-    title: "why use react?",
-    content: "react is a favorite js lib among engineers",
-  },
-  {
-    title: "how do you use React?",
-    content: "you use react by creating components",
-  },
-];
+// const items = [
+//   {
+//     title: "what is React ?",
+//     content: "react is a front end js framework",
+//   },
+//   {
+//     title: "why use react?",
+//     content: "react is a favorite js lib among engineers",
+//   },
+//   {
+//     title: "how do you use React?",
+//     content: "you use react by creating components",
+//   },
+// ];
 
-export default () => {
+const App = () => {
   return (
     <div>
       {/* <Accordion items={items} /> */}
@@ -25,3 +25,5 @@ export default () => {
     </div>
   );
 };
+
+export default App;
